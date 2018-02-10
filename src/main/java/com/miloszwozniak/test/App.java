9919d01694d85531bd65e57bad028b85e3af5951
Milosz Wozniak <milosz.wozniak@gmail.com>
2018-02-10 17:03:28 +0100

    Initial commit

---
+++ src/main/java/com/miloszwozniak/test/App.java
@@ -0,0 +1,13 @@
+package com.miloszwozniak.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
