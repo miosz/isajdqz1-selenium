028ddd9c363c26179cc007fb1de3f37ff95f20c2
Milosz Wozniak <milosz.wozniak@gmail.com>
2018-02-10 17:48:50 +0100

    Google test1 completed

---
+++ src/test/java/com/miloszwozniak/test/Test1.java
@@ -0,0 +1,36 @@
+package com.miloszwozniak.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.Assert.assertTrue;
+
+public class Test1 {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chrome/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void openPage(){
+        driver.get("https://www.google.pl");
+        WebElement buttonNameBtnk = driver.findElement(By.xpath("//input[@name='btnK']"));
+        assertTrue("Button name=bnK hasn't been found", buttonNameBtnk.isDisplayed());
+
+    }
+
+    @After
+    public void closePage(){
+        driver.close();
+    }
+
+}
